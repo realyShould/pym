5452b2eac83e4982ffbfdea15bdb317c399ed48e
realyShould <77869589+realyShould@users.noreply.github.com>
2021-01-31 18:18:31 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -13,31 +13,70 @@ class Calk(object):
         except ZeroDivisionError:
             print(Fore.YELLOW)
             print('\tChoose other numbers')
+    def divisionOfNumbers(self,a,b):
+        try:
+            c=a/b
+            print(Fore.GREEN)
+            print('Answer:', c)
+        except ZeroDivisionError:
+            print(Fore.YELLOW)
+            print('\tChoose other numbers')
+    def additionOfNumbers(self,a,b):
+        try:
+            c=a+b
+            print(Fore.GREEN)
+            print('Answer:', c)
+        except ZeroDivisionError:
+            print(Fore.YELLOW)
+            print('\tChoose other numbers')
+    def subtractionOfNumbers(self,a,b):
+        try:
+            c=a-b
+            print(Fore.GREEN)
+            print('Answer:', c)
+        except ZeroDivisionError:
+            print(Fore.YELLOW)
+            print('\tChoose other numbers')
+
 
 calk = Calk(False)
-            
-def menu():
-    print(Fore.MAGENTA)
-    print('\t\t█▀▀█ █░░█ █   █ Made by realyShould')
-    print('\t\t█░░█ █▄▄█ █▀▄▀█')
-    print('\t\t█▀▀▀ ▄▄▄█ █░░░█ for test')
+print(Fore.MAGENTA)
+print('\t\t█▀▀█ █░░█ █   █ Made by realyShould')
+print('\t\t█░░█ █▄▄█ █▀▄▀█')
+print('\t\t█▀▀▀ ▄▄▄█ █░░░█ for test')
+def menu(): 
     print(Fore.RED)
     print('\tChoose')
     print(Fore.CYAN,'[1]Calk')
-    print(Fore.RED)
+    print(Fore.MAGENTA)
     try:
         
         userInput = int(input('input:'))
         if userInput == 1:
             first = int(input('A:'))
-            action = input('Action:')
+            action = input('Action or q:')
+            if action == 'q':
+                menu()
             second = int(input('B:'))
             if action == '*':
                 calk.productOfNumbers(first,second)
                 menu()
-
+            elif action == '/':
+                calk.divisionOfNumbers(first,second)
+                menu()
+            elif action == '-':
+                calk.subtractionOfNumbers(first,second)
+                menu()
+            elif action == '+':
+                calk.additionOfNumbers(first,second)
+                menu()
+            else:
+                print(Fore.YELLOW, 'Wrong action')
+                menu()
+        elif userInput == 0:
+            quit()
     except ValueError:
-        print(Back.RED, Fore.YELLOW)
+        print(Fore.YELLOW)
         print('\tOnly numbers')
         print(Style.RESET_ALL)
         menu()
